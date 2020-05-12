159692ecae7d17c73dac41d8e6e6e40cb3d08c02
soramin <44796681+soramin@users.noreply.github.com>
2020-05-12 18:39:28 +0900

    Add files via upload

---
+++ DVDMember.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace List5_1
+{
+    /// <summary>
+    /// DVD会員管理クラス
+    /// </summary>
+ 
+    class DVDMember
+    {
+        //会員ID管理用変数 (フィールド)
+        private string _id = string.Empty;
+
+        //会員番号用プロパティ
+        public string ID
+        {
+            get
+            {
+                return this._id;
+            }
+
+            set
+            {
+                this._id = value;
+            }
+        }
+
+        public void GetID()
+        {
+            Console.WriteLine(this._id);
+        }
+        static void Main(string[] args)
+        {
+            //インスタンス生成
+            DVDMember member = new DVDMember();
+
+            member.ID = "000-00001";
+
+            string strID = member.GetID();
+
+            Console.WriteLine(strID);
+        }
+    }
+}
---
+++ List5_1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace List2-1
+/// <summary>
+/// DVD会員管理クラス
+/// </summary>
+
+class DVDMember
+{
+    //会員ID管理用変数
+    private string _id = string.Empty;
+    
+    //会員番号用プロパティ
+    public string ID
+    {
+        get
+        {
+            return this._id;
+        }
+
+        set
+        {
+            this._id = value;
+        }
+    }
+
+    public void GetID()
+    {
+        Console.WriteLine(this._ID);
+    }
+    static void Main(string[] args)
+    {
+        //インスタンス生成
+        DVDMember member = new DVDMember();
+
+        member.ID = "000-00001";
+
+        string strID = member.GetID();
+
+        Console.WriteLine(strID);
+    }
+}
